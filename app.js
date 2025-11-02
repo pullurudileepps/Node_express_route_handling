6cb26a450a1f69e91f3432181396232342a807d3
pullurudileepps <157283596+pullurudileepps@users.noreply.github.com>
2025-11-02 17:44:11 +0530

    Question 3 code

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ app.use(requestLoggerMiddleware);
 
 function requestLoggerMiddleware(req, res, next) {
   // Your code goes here.
+  //Question2
   console.log(`${new Date().toISOString()} - ${req.method} request received`)
   next();
 } 
 //Your code goes here.
-
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
@@ -19,6 +19,22 @@ app.get("/greet", (req, res) => {
     const name = req.query.name || `Guest`;
     res.send(`Hello, ${name}!`)
 })
+
+app.get("/validate-number", (req, res) => {
+  // Your code goes here.
+  try {
+    const number = parseInt(req.query.number)
+
+    if(Number.isInteger(number) && number > 0) {
+        res.send("Success!");
+    } else {
+        res.status(400).send("Invalid positive integer")
+    }
+
+  } catch(err) {
+    res.status(500).send(err)
+  }
+});
 
 app.use((req,res) => {
     res.status(401).json({message: "Route not found"})
