7bd067a9d57bc7a0078d76281345a95340f343fc
pullurudileepps <157283596+pullurudileepps@users.noreply.github.com>
2025-11-04 23:36:21 +0530

    db

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+require("./utils/index.js")
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
--- utils/index.js
+++ utils/index.js
@@ -1,2 +1,11 @@
-/*******************This file intentional kept empty . 
- * this is just a placeholder file that's kept here to represent  ideal folder structure**************/ 
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://pulluru_dileep_1729:syDBLMxnrIks3NwC@cluster.idkuw6p.mongodb.net/?appName=Cluster",
+  {}
+);
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+  console.log("Connection to MongoDB is successful");
+});
